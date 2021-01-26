694ec916175dc9b02a4e963e7cab9f5a2965367e
Ajay Arjun Shivanagol <ajay.as@appnomic.com>
2021-01-26 18:01:56 +0530

    logo change

--- visual/src/App.js
+++ visual/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import API from './API/API';
+import background from "./img/music2.webp";
 
 function App() {
   return (
     <div className="App">
+         <div style={{ backgroundImage: `url(${background})` }}>
       <header className="App-header">
         <API />
       </header>
+      </div>
     </div>
   );
 }
